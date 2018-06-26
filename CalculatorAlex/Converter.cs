e2689f92e3c20a8a764489fc0e1f642c25d7fe8f
P4ll <cornelius.weling@gmail.com>
2018-06-26 20:10:07 +0400

    конвертер и парсер

---
+++ CalculatorAlex/Converter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAlex
+{
+    class Converter
+    {
+        private static SortedDictionary<string, char> repOperators = new SortedDictionary<string, char>
+        {
+            {"умножить на", '+'},
+            {"делить на", '-'}
+        };
+        public static string ConvertString(string str)
+        {
+            foreach (var pair in repOperators)
+                str.Replace(pair.Key, pair.Value.ToString());
+            return str;
+        }
+    }
+}
---
+++ CalculatorAlex/TextRecognizer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace CalculatorAlex
+{
+    class TextRecognizer
+    {
+        static async Task RecoFromMicrophoneAsync()
+        {
+            var subscriptionKey = "9967243d0ffe4d1c99f0a824d1aa5da5";
+            var region = "westus";
+
+            var factory = SpeechFactory.FromSubscription(subscriptionKey, region);
+
+            using (var recognizer = factory.CreateSpeechRecognizer("ru-RU"))
+            {
+                Console.WriteLine("Say something...");
+                var result = await recognizer.RecognizeAsync();
+
+                if (result.RecognitionStatus != RecognitionStatus.Recognized)
+                {
+                    Console.WriteLine($"There was an error, status {result.RecognitionStatus.ToString()}, reason {result.RecognitionFailureReason}");
+                }
+                else
+                {
+                    Console.WriteLine($"We recognized: {result.Text}");
+                }
+                Console.WriteLine("Please press a key to continue.");
+                //Console.ReadLine();
+            }
+        }
+    }
+}
