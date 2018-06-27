1182a74d4eb3f66b0be3d4624e8188a21ffce86c
Azatalion <azatalion@gmail.com>
2018-06-27 13:50:03 +0400

    Update EquationParser.cs

--- CalculatorAlex/Source/EquationParsers/EquationParser.cs
+++ CalculatorAlex/Source/EquationParsers/EquationParser.cs
@@ -8,25 +8,34 @@ namespace CalculatorAlex
 {
     public class EquationParser
     {
+
         public static List<string> Steps(string expression)
         {
             List<string> output = new List<string>();
             var parts = expression.Split(' ');
             double a = double.Parse(parts[0]);
-            for (int i = 1; i < parts.Length - 1; i += 2)
+            for (int i = 1; i < parts.Length; i++)
             {
-                string step = a + " " + parts[i] + " " + parts[i + 1] + " = ";
-                double b = double.Parse(parts[i + 1]);
-                if (parts[i] == "+")
-                    a += b;
-                else if (parts[i] == "-")
-                    a -= b;
-                else if (parts[i] == "*")
-                    a *= b;
+                string step = a + " " + parts[i][0] + " " + parts[i].Substring(1) + " = ";
+                double b;
+                if (double.TryParse(parts[i].Substring(1), out b))
+                {
+                    if (parts[i][0] == '+')
+                        a += b;
+                    else if (parts[i][0] == '-')
+                        a -= b;
+                    else if (parts[i][0] == '*')
+                        a *= b;
+                    else
+                        a /= b;
+                    step += a;
+                    output.Add(step);
+                }
                 else
-                    a /= b;
-                step += a;
-                output.Add(step);
+                {
+                    output.Add("Ошибка - " + parts[i].Substring(1) + ".");
+                    break;
+                }
             }
             return output;
         }
