1ba64f567437d0bc527fa286f8bf55088ff0a2ca
P4ll <cornelius.weling@gmail.com>
2018-06-29 11:06:47 +0400

    Замена литерального представления больших чисел.

--- CalculatorAlex/Source/SpeechParsers/Converter.cs
+++ CalculatorAlex/Source/SpeechParsers/Converter.cs
@@ -9,6 +9,7 @@ namespace CalculatorAlex
     public class Converter
     {
         private readonly Dictionary<string, string> _operationsDict;
+        private readonly Dictionary<string, double> _bigNumberDict;
 
         public Converter()
         {
@@ -25,16 +26,61 @@ namespace CalculatorAlex
                 {"плюс", "+"},
                 {"минус", "-"},
                 {"minus", "-"},
-                {"plus", "+"}
+                {"plus", "+"},
+                {",", "."}
             };
 
+            _bigNumberDict = new Dictionary<string, double>
+            {
+                {"млн", 1000000},
+                {"млрд", 1000000000}
+            };
         }
+        private string TransformBigNumbers(string str)
+        {
+            var pieces = str.Split(' ').ToList();
+            for (int i = 0; i < pieces.Count; ++i)
+            {
+                if (_bigNumberDict.ContainsKey(pieces[i]))
+                {
+                    double t = _bigNumberDict[pieces[i]];
 
+                    double value = 0;
+                    if (i != 0 && Double.TryParse(pieces[i - 1], out value))
+                    {
+                        t *= value;
+                        pieces[i] = t.ToString();
+                        pieces.RemoveAt(i - 1);
+                    }
+                }
+            }
+
+            for (int i = 1; i < pieces.Count; ++i)
+            {
+                double value = 0, value2 = 0;
+                if (Double.TryParse(pieces[i - 1], out value) && Double.TryParse(pieces[i], out value2))
+                {
+                    value2 += value;
+                    pieces[i] = value2.ToString();
+                    pieces[i - 1] = "";
+                }
+            }
+
+            var sb = new StringBuilder();
+            foreach (var p in pieces)
+            {
+                if (p == "")
+                    continue;
+                sb.Append(p + " ");
+            }
+            return sb.ToString().Trim(' ');
+        }
         public string ConvertTextToEquation(string str)
         {
             str = str.ToLower();
             foreach (var pair in _operationsDict)
                 str = str.Replace(pair.Key, pair.Value);
+            str = TransformBigNumbers(str);
             char[] symbols = str.ToCharArray();
             bool flag = false;
             for (int i = 0; i < symbols.Length; ++i)
