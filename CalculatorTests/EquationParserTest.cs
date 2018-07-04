df5c1711b64ff29c4756584d3d98b0999f5d3356
Azatalion <azatalion@gmail.com>
2018-07-04 13:09:49 +0400

    Atempt

---
+++ CalculatorTests/EquationParserTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using CalculatorAlex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class EquationParserTest
+    {
+
+        [TestMethod]
+        public void StepsTest1()
+        {
+            var actual = EquationParser.Steps("3 + 5");
+            var excepted = new List<string>() { "3 + 5 = 8" };
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
